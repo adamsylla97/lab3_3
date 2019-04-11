05fcc36df70f89946343945fb4017320950bc7a5
adamsylla97 <209474@edu.p.lodz.pl>
2019-04-11 17:03:35 +0200

    *added test confirmShouldThrowOrderExpiredException

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,4 +1,19 @@
+import edu.iis.mto.time.Order;
+import edu.iis.mto.time.OrderExpiredException;
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class OrderTest {
 
+    @Test (expected = OrderExpiredException.class)
+    public void confirmShouldThrowOrderExpiredException(){
+        Order order = new Order();
+        order.submit();
+        order.confirm();
+    }
 
 }
